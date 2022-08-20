26536ae3a0844098876d742e3770b051d924dc2f
Scharss <scharss@gmail.com>
2022-08-20 10:46:22 -0500

    Add files via upload

---
+++ src/index2.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import {Greeting, UserCard} from './Greeting';
+import {Button} from './Button.js'
+import {TaskCard} from './Task.js'
+import {Posts} from './Posts.js'
+const root = ReactDom.createRoot(document.getElementById('root'));
+
+{/*   */}
+
+
+		
+root.render(<>
+
+
+</>)

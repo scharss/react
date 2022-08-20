3e76c2f820a225ba2a8cb31714272202ac31dfcd
Scharss <scharss@gmail.com>
2022-08-20 11:37:13 -0500

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 
+
+
 export function Counter(){
 	
 	const [counter, setCounter] = useState(10)
@@ -10,7 +12,10 @@ export function Counter(){
 	
 	<h1>Counter: {counter}</h1>
 	<button onClick={()=>setCounter(counter+1)}>Sumar</button>
-	
+	<button onClick={()=>setCounter(counter-1)}>Restar</button>
+	<button onClick={()=>{setCounter(1000)}}>Reiniciar</button>
 	</div>
 	       )
 }
+
+
